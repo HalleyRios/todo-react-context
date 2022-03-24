c832274465cb2fceeb74d304341902dda9213f96
Halley Rios <HalleyRios@gmail.com>
2022-03-24 00:12:04 -0300

    chore: adds `Router`

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../view/Home';
+import AddTodo from '../view/AddTodo';
+import EditTodo from '../view/EditTodo';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<AddTodo />} />
+          <Route path="/edit/:id" element={<EditTodo />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
